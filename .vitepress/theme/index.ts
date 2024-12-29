e3ae6f2fd32409750c3deaad354fcddc61f7da3d
AhmedOsman101 <trollingosman101@outlook.com>
2024-12-29 12:39:03 +0200

    Updated Files 2024-12-29@12:39PM

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,12 +1,4 @@
 import DefaultTheme from "vitepress/theme-without-fonts";
-import ScriptX from "vue-scriptx";
-import Ads from "vue-google-adsense";
 import "./main.css";
 
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app, router, siteData }) {
-    app.use(ScriptX);
-    app.use(Ads.InArticleAdsense);
-  },
-};
+export default DefaultTheme;

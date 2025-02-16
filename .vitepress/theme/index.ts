a62903a8395e44e9314d54481833bc343874a2a7
AhmedOsman101 <trollingosman101@outlook.com>
2025-02-16 14:32:30 +0200

    Updated Files 2025-02-16@02:32PM

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,13 +1,13 @@
-import DefaultTheme from "vitepress/theme-without-fonts";
 import vitepressNprogress from "vitepress-plugin-nprogress";
-import { EnhanceAppContext } from "vitepress/dist/client/index.js";
+import type { EnhanceAppContext } from "vitepress/dist/client/index.js";
+import DefaultTheme from "vitepress/theme-without-fonts";
 
 import "vitepress-plugin-nprogress/lib/css/index.css";
 import "./main.css";
 
 export default {
-  ...DefaultTheme,
-  enhanceApp: (ctx: EnhanceAppContext) => {
-    vitepressNprogress(ctx);
-  },
+	...DefaultTheme,
+	enhanceApp: (ctx: EnhanceAppContext) => {
+		vitepressNprogress(ctx);
+	},
 };
